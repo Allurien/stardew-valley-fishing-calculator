47f253b5a90e1aa558a20415b7585c5de4e76d6e
Steffany <kielen@gmail.com>
2018-08-25 21:24:29 -0700

    added dummy fish data

---
+++ fish.js
@@ -0,0 +1,33 @@
+// Fish Data
+var fish = {
+    halibut: {
+        difficulty: 50,
+        normalXP: 19.7,
+        silverXP: 22.6,
+        goldXP: 25.6,
+    },
+    herring: {
+        difficulty: 25,
+        normalXP: 11.3,
+        silverXP: 14.3,
+        goldXP: 17.3,
+    },
+    sardine: {
+        difficulty: 30,
+        normalXP: 13,
+        silverXP: 16,
+        goldXP: 19,
+    },
+    sunfish: {
+        difficulty: 30,
+        normalXP: 13,
+        silverXP: 16,
+        goldXP: 19,
+    }
+
+
+
+
+
+
+}
