0cf709e96e4408417a945c8603ae8fdc1a0146e3
Steffany <kielen@gmail.com>
2018-09-10 02:39:35 -0700

    completed basic oop logic

--- fish.js
+++ fish.js
@@ -2,27 +2,19 @@
 var fish = {
     Halibut: {
         difficulty: 50,
-        normalXP: 19.7,
-        silverXP: 22.6,
-        goldXP: 25.6,
+        basePrice: 80
     },
     Herring: {
         difficulty: 25,
-        normalXP: 11.3,
-        silverXP: 14.3,
-        goldXP: 17.3,
+        basePrice: 30
     },
     Sardine: {
         difficulty: 30,
-        normalXP: 13,
-        silverXP: 16,
-        goldXP: 19,
+        basePrice: 40
     },
     Sunfish: {
         difficulty: 30,
-        normalXP: 13,
-        silverXP: 16,
-        goldXP: 19,
+        basePrice: 30
     }
 
 
--- mainoop.js
+++ mainoop.js
@@ -1,16 +1,176 @@
+$(document).ready(goGoApp);
+function goGoApp(){
+    const fishCalc = new Calculator;
+    fishCalc.addWizard();
+    $(".addition-item").click(fishCalc.addWizard);
+}
 class Fish {
-    constructor(fishName, difficulty, quality, basePrice){
+    constructor(fishName, xp, basePrice){
         this.name = fishName;
-        this.difficulty = difficulty;
-        this.quality = quality;
+        this.xp = xp;
         this.price = basePrice;
-        this.fishElement = null;
     }
-    calculateFishXP(difficulty, quality){
+    render(){
+        const fishElement = $("<div>").addClass("calculator__display__fish");
+        const fishName = $("<p>").text(this.name).addClass("calculator__display__fish__item");
+        const fishXP = $("<p>").text(`Your ${this.name} is worth ${this.xp}xp`).addClass("calculator__display__fish__item");
+        const fishPrice = $("<p>").text(`Your ${this.name} is worth ${this.price} gold.`).addClass("calculator__display__fish__item");
+        $(fishElement).append(fishName, fishXP, fishPrice);
+        $(".calculator__display").append(fishElement);
+    }
+}
+class Item {
+    constructor(fishName, basePrice, xp){
+        this.name = fishName;
+        this.price = basePrice;
+        this.xp = xp;
+    }
+    render(){
+        const itemElement = $("<div>").addClass("calculator__display__fish");
+        const itemName = $("<p>").text(this.name).addClass("calculator__display__fish__item");
+        const itemXP = $("<p>").text(`Your ${this.name} is worth ${this.xp}xp`).addClass("calculator__display__fish__item");
+        const itemPrice = $("<p>").text(`Your ${this.name} is worth ${this.price} gold.`).addClass("calculator__display__fish__item");
+        $(itemElement).append(itemName, itemXP, itemPrice);
+        $(".calculator__display").append(itemElement);
+    }
+}
+class Calculator {
+    constructor(){
+        this.addedItems = [];
+        this.addFish = this.addFish.bind(this);
+        this.addItem = this.addItem.bind(this);
+    }
+    addWizard(){
+        const fishWiz = new FishWizard(this.addFish, this.addItem);
+        $(".start-item-selector").click(fishWiz.startCalculator);
+        $(".start-fish-selector").click(fishWiz.startFishSelector);
+        $(".start-quality-selector").click(fishWiz.startQualitySelector);
+        $(".start-modifier-selector").click(fishWiz.startModifierSelector);
+        $(".start-final-calculation").click(fishWiz.startFinalCalculation);
+    }
+    addFish(fishName, difficulty, quality, basePrice){
+        const fish = new Fish(fishName, difficulty, quality, basePrice)
+        const makeFish = fish.render();
+        $(".calculator__display").append(makeFish);
+        this.addedItems.push(fish);
+        console.log(this.addedItems, "parent added items");
+    }
+    addItem(fishName, basePrice, xp){
+        const item = new Item(fishName, basePrice, xp)
+        const makeItem = item.render();
+        $(".calculator__display").append(makeItem);
+        this.addedItems.push(item);
+        console.log(this.addedItems, "parent added items");
+    }
+    render(){
+    }
+}
+
+
+class FishWizard {
+// Controls the options for fish selection and value setting
+    constructor(addFish, addItem){
+        this.price = null;
+        this.fishElement = null;
+        this.addedItems = [];
+        this.calculationInfo = {
+            "itemType": null,
+            "itemBaseXP": null,
+            "fishType": null,
+            "difficulty": null,
+            "quality": null,
+            "modifiers": null
+        }
+        this.startFishSelector = this.startFishSelector.bind(this);
+        this.startQualitySelector = this.startQualitySelector.bind(this);
+        this.startModifierSelector = this.startModifierSelector.bind(this);
+        this.startFinalCalculation = this.startFinalCalculation.bind(this);
+        this.calculateFishXP = this.calculateFishXP.bind(this);
+        this.addFish = addFish;
+        this.addItem = addItem;
+
+    }
+    startCalculator(){
+        console.log('started fish wizard!');
+        $('.calculator__text__item').text('What did you catch?');
+        $('.start-item-selector').detach();
+        $(".item-selector").removeClass("display-none");
+    }
+    startFishSelector(){
+        const item = $(".item-selector :selected").text();
+        $('.item-selector').detach();
+        if(item === "Trash" || item === "Algae"){
+            this.calculationInfo.itemType = item;
+            this.calculationInfo.itemBaseXP = 3;
+            $('.calculator__text__item').text('Did you have any catch modifiers? Choose your highest one.');
+            $(".modifiers").removeClass("display-none");
+            return;
+        }
+        if(item === "Crab Pot"){
+            this.calculationInfo.itemType = item;
+            this.calculationInfo.itemBaseXP = 5;
+            // this.addedItems.push({"item": item, "xp": 5});
+            this.addItem("name", "price", this.calculationInfo.itemBaseXP);
+            $(".calculator__display").removeClass("display-none");
+            return;
+        }
+        if(item==="Fish"){
+            this.calculationInfo.itemType = item;
+            $('.calculator__text__item').text('What fish did you catch?');
+            $(".fish-selector").removeClass("display-none");
+            return;
+        }
+    }
+    startQualitySelector(){
+        const fishType = $(".fish-selector :selected").text();
+        this.calculationInfo.fishType = fishType;
+        $(".fish-selector").detach();
+        $('.calculator__text__item').text('What quality fish did you catch?');
+        $(".fish-quality").removeClass("display-none");
+    }
+    startModifierSelector(){
+        if(this.calculationInfo.itemType === "Fish"){
+            const quality = $("input[name=quality]:checked").val();
+            this.calculationInfo.quality = quality;
+        }
+        $(".fish-quality").detach();
+        $('.calculator__text__item').text('Did you have any catch modifiers? Choose your highest one.');
+        $(".modifiers").removeClass("display-none");
+    }
+    startFinalCalculation(){
+        const modifiers = $("input[name=modifiers]:checked").val();
+        let finalXP = null;
+        if(modifiers === "None"){
+            this.calculationInfo.modifiers = 1;
+        }
+        if(modifiers === "Treasure"){
+            this.calculationInfo.modifiers = 2.2;
+        }
+        if(modifiers === "Perfect"){
+            this.calculationInfo.modifiers = 2.4;
+        }
+        if(modifiers === "Legendary"){
+            this.calculationInfo.modifiers = 5;
+        }
+        $(".modifiers").detach();
+        if(this.calculationInfo.itemType === "Fish"){
+            this.calculationInfo.difficulty = fish[this.calculationInfo.fishType].difficulty;
+            this.price = fish[this.calculationInfo.fishType].basePrice;
+            console.log(this.calculationInfo.difficulty, "Difficulty");
+            finalXP = this.calculateFishXP(this.calculationInfo.difficulty, this.calculationInfo.quality, this.calculationInfo.modifiers);
+            this.addFish(this.calculationInfo.fishType, finalXP, this.price);
+        }
+        if(this.calculationInfo.itemType === "Trash" || this.calculationInfo.itemType === "Algae"){
+            finalXP = Math.round(((3*this.calculationInfo.modifiers) + 0.00001) * 100) / 100;
+            this.addItem(this.calculationInfo.itemType, 0, finalXP);
+        }
+        $(".calculator__display").removeClass("display-none");
+    }
+    calculateFishXP(difficulty, quality, modifiers){
         const normal = "normal";
         const silver = "silver";
         const gold = "gold";
-        const baseXP = null;
+        let baseXP = null;
         if(quality === normal){
             baseXP = 3 + (difficulty/3);
         }
@@ -20,82 +180,10 @@ class Fish {
         if(quality === gold){
             baseXP = 9 + (difficulty/3);
         }
-        baseXP = Math.round((baseXP + 0.00001) * 100) / 100;
-        return baseXP;
-    }
-    calculateFishValue(quality, basePrice){
-
-    }
-    render(){
-        this.fishElement = $("<div>").hasClass("calculator__display__fish");
-        this.fishName = $("<p>").text(this.name).hasClass("calculator__display__fish__item");
-        this.fishXP = $("<p>").text(this.calculateFishValue(this.difficulty, this.quality)).hasClass("calculator__display__fish__item");
-        this.fishPrice = $("<p>").text("result of price processing here").hasClass("calculator__display__fish__item");
-        $(this.fishElement).append(this.fishName, this.fishXP, this.fishPrice);
-        $(".calculator__display").append(this.fishElement);
-
-    }
-}
-class Calculator {
-    constructor(){
-
-    }
-    addFish(fishName, difficulty, quality, basePrice){
-        const fish = new Fish(fishName, difficulty, quality, basePrice)
-        const makeFish = Fish.render();
-
+        let finalXP = baseXP*modifiers;
+        finalXP = Math.round((finalXP + 0.00001) * 100) / 100;
+        console.log(finalXP, "finalxp");
+        return finalXP;
     }
 }
 
-const addClickHandlers=()=>{
-    $(".start-item-selector").click(startCalculator);
-    $(".start-fish-selector").click(startFishSelector);
-    $(".start-quality-selector").click(startQualitySelector);
-    $(".start-modifier-selector").click(startModifierSelector);
-    $(".start-final-calculation").click(startFinalCalculation);
-}
-//Click Handlers
-const startCalculator = () => {
-    $(".calculator__text").addClass("display-none");
-    $(".item-selector").removeClass("display-none");
-}
-const startFishSelector = () => {
-    $(".item-selector").addClass("display-none");
-    const item = $(".item-selector :selected").text();
-    let itemBaseXP = null;
-    if(item === "Trash" || item === "Algae"){
-        calculationInfo.itemType = item;
-        itemBaseXP = 3;
-        $(".modifiers").removeClass("display-none");
-        return;
-    }
-    if(item === "Crab Pot"){
-        calculationInfo.itemType = item;
-        itemBaseXP = 5;
-        addedItems.push({"item": item, "xp": 5});
-        $(".calculator__display").removeClass("display-none");
-        $(".calculator__display").append(`<p>Your ${item} is worth 5xp.</p>`)
-        return;
-    }
-
-    if(item==="Fish"){
-        calculationInfo.itemType = item;
-
-        $(".fish-selector").removeClass("display-none");
-        return;
-    }
-
-}
-const startQualitySelector = () => {
-    $(".fish-selector").addClass("display-none");
-    $(".fish-quality").removeClass("display-none");
-    const quality = $("input[name=quality]:checked").val();
-}
-const startModifierSelector = () => {
-    $(".fish-quality").addClass("display-none");
-    $(".modifiers").removeClass("display-none");
-}
-const startFinalCalculation = () => {
-    $(".modifiers").addClass("display-none");
-    $(".calculator__display").removeClass("display-none");
-}
