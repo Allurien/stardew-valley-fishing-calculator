926c3ca9f49939fb9ad075dbe41833b8431c7960
Steffany <kielen@gmail.com>
2018-09-11 10:45:07 -0700

    completed conversion to dynamic jquery, adjusted output so that multiple items can be added, and then cleared, cleaned up console logs

--- mainoop.js
+++ mainoop.js
@@ -40,54 +40,41 @@ class Calculator {
         this.addItem = this.addItem.bind(this);
         this.addWizard = this.addWizard.bind(this);
         this.addAdditionalWizard = this.addAdditionalWizard.bind(this);
-        // this.createItemSelectorElement = this.createItemSelectorElement.bind(this);
+        this.resetCalculator = this.resetCalculator.bind(this);
     }
     addWizard(){
         const fishWiz = new FishWizard(this.addFish, this.addItem);
-        console.log("adding a new wizard");
         $(".start-item-selector").click(fishWiz.startCalculator);
-        // $(".start-fish-selector").click(fishWiz.startFishSelector);
-        $(".start-quality-selector").click(fishWiz.startQualitySelector);
-        $(".start-modifier-selector").click(fishWiz.startModifierSelector);
-        $(".start-final-calculation").click(fishWiz.startFinalCalculation);
         $(".addition-item").click(this.addAdditionalWizard);
+        $(".reset-calculator").click(this.resetCalculator);
+        if($(".calculator__display").hasClass("display-none")){
+            return;
+        }
         if($(".calculator__display__buttons").hasClass("display-none")){
             fishWiz.startCalculator();
         }
     }
     addAdditionalWizard(){
         $(".calculator__display__buttons").addClass("display-none");
-        
+        $(".calculator__selections").empty();
         this.addWizard();
-        
     }
-    // createItemSelectorElement(){
-    //     const itemSelector = $("<div>").addClass("item-selector");
-    //     const selectBox = $("<select>").addClass("item-selector__dropdown")
-    //                         .append(itemSelectorOptions.map(function(item) {
-    //                             return $('<option/>', {
-    //                                 value: item,
-    //                                 text: item
-    //                             })
-    //                             }))
-    //                         .attr("id", "item-selector__dropdown");
-    //     const itemSelectButton = $("<button>Next</button>").addClass("start-fish-selector").live("click", this.startFishSelector);
-    //     $(itemSelector).append(selectBox, itemSelectButton);
-    //     $(".calculator__selections").append(itemSelector);
-    // }
+    resetCalculator(){
+        this.addedItems = [];
+        $(".calculator__display").empty();
+        this.addAdditionalWizard();
+    }
     addFish(fishName, difficulty, quality, basePrice){
         const fish = new Fish(fishName, difficulty, quality, basePrice)
         const makeFish = fish.render();
         $(".calculator__display").append(makeFish);
         this.addedItems.push(fish);
-        console.log(this.addedItems, "parent added items");
     }
     addItem(fishName, basePrice, xp){
         const item = new Item(fishName, basePrice, xp)
         const makeItem = item.render();
         $(".calculator__display").append(makeItem);
         this.addedItems.push(item);
-        console.log(this.addedItems, "parent added items");
     }
     render(){
     }
@@ -96,7 +83,6 @@ class Calculator {
 
 class FishWizard {
 // Controls the options for fish selection and value setting
-//TODO: Complete converting functions to dynamically create the dom elements
     constructor(addFish, addItem){
         this.price = null;
         this.fishElement = null;
@@ -119,14 +105,12 @@ class FishWizard {
         this.addItem = addItem;
 
     }
-    
     startCalculator(){
         const itemSelectorOptions = ["Select Item", "Trash", "Algae", "Crab Pot", "Fish"];
-        console.log('started fish wizard!');
         $('.calculator__text__item').text('What did you catch?');
         $('.start-item-selector').detach();
         const itemSelector = $("<div>").addClass("item-selector");
-        const selectBox = $("<select>").addClass("item-selector__dropdown")
+        const itemSelectBox = $("<select>").addClass("item-selector__dropdown")
                             .append(itemSelectorOptions.map(function(item) {
                                 return $('<option/>', {
                                     value: item,
@@ -134,19 +118,18 @@ class FishWizard {
                                 })
                                 }))
                             .attr("id", "item-selector__dropdown");
-        const itemSelectButton = $("<button>Next</button>").addClass("start-fish-selector").click(this.startFishSelector);
-        $(itemSelector).append(selectBox, itemSelectButton);
+        const itemSubmitButton = $("<button>Next</button>").addClass("start-fish-selector").click(this.startFishSelector);
+        $(itemSelector).append(itemSelectBox, itemSubmitButton);
         $(".calculator__selections").append(itemSelector);
     }
     startFishSelector(){
-        console.log('starting fish selector');
         const item = $(".item-selector :selected").text();
         $('.item-selector').detach();
         if(item === "Trash" || item === "Algae"){
             this.calculationInfo.itemType = item;
             this.calculationInfo.itemBaseXP = 3;
             $('.calculator__text__item').text('Did you have any catch modifiers? Choose your highest one.');
-            $(".modifiers").removeClass("display-none");
+            this.startModifierSelector();
             return;
         }
         if(item === "Crab Pot"){
@@ -154,6 +137,7 @@ class FishWizard {
             this.calculationInfo.itemBaseXP = 5;
             // this.addedItems.push({"item": item, "xp": 5});
             this.addItem("name", "price", this.calculationInfo.itemBaseXP);
+            $(".calculator__selections").empty();
             $(".calculator__display").removeClass("display-none");
             $(".calculator__display__buttons").removeClass("display-none");
             return;
@@ -161,8 +145,14 @@ class FishWizard {
         if(item==="Fish"){
             this.calculationInfo.itemType = item;
             $('.calculator__text__item').text('What fish did you catch?');
-            $(".fish-selector").removeClass("display-none");
-            return;
+            const fishSelector = $("<div>").addClass("fish-selector");
+            const fishSelectBox = $("<select>").addClass("fish-selector__dropdown").attr("id", "item-selector__dropdown");
+            $.each(fish, function(i) {
+                $(fishSelectBox).append($('<option>').val(i).text(i))
+                });
+            const fishSubmitButton = $("<button>Next</button>").addClass("start-fish-selector").click(this.startQualitySelector);
+            $(fishSelector).append(fishSelectBox, fishSubmitButton);
+            $(".calculator__selections").append(fishSelector);
         }
     }
     startQualitySelector(){
@@ -170,7 +160,16 @@ class FishWizard {
         this.calculationInfo.fishType = fishType;
         $(".fish-selector").detach();
         $('.calculator__text__item').text('What quality fish did you catch?');
-        $(".fish-quality").removeClass("display-none");
+        const qualitySelector = $("<div>").addClass("fish-quality");
+        const normalLabel = $("<label>Normal</label>");
+        const normalRadio = $('<input type="radio" name="quality" value="normal">');
+        const silverLabel = $("<label>Silver</label>");
+        const silverRadio = $('<input type="radio" name="quality" value="silver">');
+        const goldLabel = $("<label>Gold</label>");
+        const goldRadio = $('<input type="radio" name="quality" value="Gold">');
+        const qualitySubmitButton = $("<button>Next</button>").addClass("start-modifier-selector").click(this.startModifierSelector);
+        $(qualitySelector).append(normalLabel, normalRadio, silverLabel, silverRadio, goldLabel, goldRadio, qualitySubmitButton);
+        $(".calculator__selections").append(qualitySelector);
     }
     startModifierSelector(){
         if(this.calculationInfo.itemType === "Fish"){
@@ -179,7 +178,18 @@ class FishWizard {
         }
         $(".fish-quality").detach();
         $('.calculator__text__item').text('Did you have any catch modifiers? Choose your highest one.');
-        $(".modifiers").removeClass("display-none");
+        const modifierSelector = $("<div>").addClass("modifiers");
+        const noneLabel = $("<label>None</label>");
+        const noneRadio = $('<input type="radio" name="modifiers" value="None">');
+        const treasureLabel = $("<label>Treasure</label>");
+        const treasureRadio = $('<input type="radio" name="modifiers" value="Treasure">');
+        const perfectLabel = $("<label>Perfect</label>");
+        const perfectRadio = $('<input type="radio" name="modifiers" value="Perfect">');
+        const legendaryLabel = $("<label>Legendary</label>");
+        const legendaryRadio = $('<input type="radio" name="modifiers" value="Legendary">');
+        const modifierSubmitButton = $("<button>Calculate</button>").addClass("start-final-calculation").click(this.startFinalCalculation);
+        $(modifierSelector).append(noneLabel, noneRadio, treasureLabel, treasureRadio, perfectLabel, perfectRadio, legendaryLabel, legendaryRadio, modifierSubmitButton);
+        $(".calculator__selections").append(modifierSelector);
     }
     startFinalCalculation(){
         const modifiers = $("input[name=modifiers]:checked").val();
@@ -200,7 +210,6 @@ class FishWizard {
         if(this.calculationInfo.itemType === "Fish"){
             this.calculationInfo.difficulty = fish[this.calculationInfo.fishType].difficulty;
             this.price = fish[this.calculationInfo.fishType].basePrice;
-            console.log(this.calculationInfo.difficulty, "Difficulty");
             finalXP = this.calculateFishXP(this.calculationInfo.difficulty, this.calculationInfo.quality, this.calculationInfo.modifiers);
             this.addFish(this.calculationInfo.fishType, finalXP, this.price);
         }
@@ -227,7 +236,6 @@ class FishWizard {
         }
         let finalXP = baseXP*modifiers;
         finalXP = Math.round((finalXP + 0.00001) * 100) / 100;
-        console.log(finalXP, "finalxp");
         return finalXP;
     }
 }
